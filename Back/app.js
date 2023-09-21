f39458063d8b472a5c5738cc220412b0f9150332
abreyaustin <abreyaustin@gmail.com>
2023-09-21 13:10:11 -0400

    Added back-end form submission handling. Added parsing with body-parser.
    Setup basic outline of form submission handling server-side. Using body-parser middleware to parse messages.
    Created outline for future addition of data process and storing in database.

--- Back/app.js
+++ Back/app.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser'); // import body-parser
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+// User body-parser to parse messages
+app.use(bodyParser.urlencoded({ extended: true}))
+app.use(bodyparser.json());
+
+// Define route for handling POST requests (form submission)
+app.post('/submit', (req, res) => {
+    // Access data from submitted form
+    const formData = req.body; 
+    // Process data from form and store in database
+        // TODO
+    
+
+    // Confirmation
+    res.status(200).send('Form submitted successfully!');
 });
 
+// Start server to listen for messages
 app.listen(PORT, () => {
     console.log('Server running at http://localhost:${PORT}/');
 });
