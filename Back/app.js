1a467dcb6f073e88bbaa7ed13c9a03d880efb4e1
abreyaustin <abreyaustin@gmail.com>
2023-09-22 16:15:40 -0400

    Added front-end POST request, fixed typo in Express application.
    Added form action to index.html that sends a POST request to the back-end when a form is submitted on the webpage. Added a console log for the Express application that prints a confirmation when data is received.
    Also fixed a small typo in app.js. "bodyparser" was used instead of "bodyParser"

--- Back/app.js
+++ Back/app.js
@@ -5,12 +5,14 @@ const PORT = process.env.PORT || 3000;
 
 // User body-parser to parse messages
 app.use(bodyParser.urlencoded({ extended: true}))
-app.use(bodyparser.json());
+app.use(bodyParser.json());
 
 // Define route for handling POST requests (form submission)
 app.post('/submit', (req, res) => {
     // Access data from submitted form
     const formData = req.body; 
+    // Log received data to the console
+    console.log('Received form data: ', formData);
     // Process data from form and store in database
         // TODO
     
