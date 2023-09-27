1aac250165700e875b6befd654e9bb814ffa5094
abreyaustin <abreyaustin@gmail.com>
2023-09-27 15:18:50 -0400

    Added setup for using MongoDB in app.js application.
    Using MongoDB as database.
    Added functionality to app.js to connect to db running locally, create a new client, and try connecting to the db

--- Back/app.js
+++ Back/app.js
@@ -3,6 +3,27 @@ const bodyParser = require('body-parser'); // import body-parser
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const { MongoClient } = require('mongodb');
+
+// Connection URI. Will need to update location once hosted remotely
+const uri = 'mongodb://localhost:27017/webpage_data';
+
+// Create a new MongoClient
+const client = new MOngoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Connect to the database
+async function connectToDatabase() {
+    try {
+        await client.connect();
+        console.log('Connected to database');
+    } catch (error) {
+        console.error('Error connecting to database: ', error);
+    }
+}
+
+// Connect to database
+connectToDatabase();
+
 // User body-parser to parse messages
 app.use(bodyParser.urlencoded({ extended: true}))
 app.use(bodyParser.json());
